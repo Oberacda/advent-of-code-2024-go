e92c856365e9cc705d185691ae36e4579f95a188
David Oberacker <developer@oberacker.me>
2024-12-01 18:11:39 +0100

    Change structure.

--- problem1_part1/main.go
+++ problem1_part1/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
 func main() {
-	inputBytes, err := os.ReadFile("./problem1/input1.txt")
+	inputBytes, err := os.ReadFile("./problem1_part1/input1.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -17,7 +19,7 @@ func main() {
 	res := numbersRegex.FindAllStringSubmatch(str, -1)
 
 	firstList := make([]int64, len(res))
-	secondMap := make(map[int64]int64)
+	secondList := make([]int64, len(res))
 	for i := range res {
 		match := res[i]
 		if len(match) != 3 {
@@ -26,24 +28,17 @@ func main() {
 		first, _ := strconv.ParseInt(match[1], 10, 64)
 		firstList[i] = first
 		second, _ := strconv.ParseInt(match[2], 10, 64)
-
-		no, ok := secondMap[second]
-		if ok {
-			secondMap[second] = no + 1
-		} else {
-			secondMap[second] = 1
-		}
+		secondList[i] = second
 
 	}
+	sort.Slice(firstList, func(i, j int) bool { return firstList[i] < firstList[j] })
+	sort.Slice(secondList, func(i, j int) bool { return secondList[i] < secondList[j] })
 
-	var result int64 = 0
+	result := 0.0
 	for i := range firstList {
-		elem := firstList[i]
-		no, ok := secondMap[elem]
-		if ok {
-			result += elem * no
-		}
+		diff := firstList[i] - secondList[i]
+		result += math.Abs(float64(diff))
 	}
 
-	fmt.Printf("%d\n", result)
+	fmt.Printf("%.0f\n", result)
 }
--- problem1_part2/main.go
+++ problem1_part2/main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 	"regexp"
-	"sort"
 	"strconv"
 )
 
 func main() {
-	inputBytes, err := os.ReadFile("./problem1/input1.txt")
+	inputBytes, err := os.ReadFile("./problem1_part2/input1.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -19,7 +17,7 @@ func main() {
 	res := numbersRegex.FindAllStringSubmatch(str, -1)
 
 	firstList := make([]int64, len(res))
-	secondList := make([]int64, len(res))
+	secondMap := make(map[int64]int64)
 	for i := range res {
 		match := res[i]
 		if len(match) != 3 {
@@ -28,17 +26,24 @@ func main() {
 		first, _ := strconv.ParseInt(match[1], 10, 64)
 		firstList[i] = first
 		second, _ := strconv.ParseInt(match[2], 10, 64)
-		secondList[i] = second
+
+		no, ok := secondMap[second]
+		if ok {
+			secondMap[second] = no + 1
+		} else {
+			secondMap[second] = 1
+		}
 
 	}
-	sort.Slice(firstList, func(i, j int) bool { return firstList[i] < firstList[j] })
-	sort.Slice(secondList, func(i, j int) bool { return secondList[i] < secondList[j] })
 
-	result := 0.0
+	var result int64 = 0
 	for i := range firstList {
-		diff := firstList[i] - secondList[i]
-		result += math.Abs(float64(diff))
+		elem := firstList[i]
+		no, ok := secondMap[elem]
+		if ok {
+			result += elem * no
+		}
 	}
 
-	fmt.Printf("%.0f\n", result)
+	fmt.Printf("%d\n", result)
 }
